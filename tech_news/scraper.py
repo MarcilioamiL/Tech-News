154da3a713d0f6160ce7119a9c0c698073be3a9a
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-11-08 23:16:41 -0300

    feat: 4 - Crie a função scrape_noticia/title

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -40,6 +40,7 @@ def scrape_noticia(html_content):
         ),
         "tags": selector.css("a[rel='tag']::text").getall(),
         "timestamp": selector.css("li.meta-date::text").get(),
+        "title": (selector.css("h1.entry-title::text").get()).strip(),
 
     }
 
