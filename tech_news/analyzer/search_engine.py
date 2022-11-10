e95c9e4a23088a70024e7e408dcaaea68aa8923d
MarcilioamiL <marcilioaugusto53@gmail.com>
2022-11-09 21:41:52 -0300

    feat: 6 - Crie a função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,30 @@
+from tech_news.database import search_news
+
+
+
+def new_forma(new_forma):
+    new_list = []
+    for i in new_forma:
+        new_formation = (i["title"], i["url"])
+        new_list.append(new_formation)
+    return new_list
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    return new_forma(news)
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+
 
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
